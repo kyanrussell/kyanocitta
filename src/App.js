01e56fd67c380c7cf1034f056c1b83db86f4d567
Kyan Russell <ksrussell@berkeley.edu>
2025-03-01 22:04:16 -0800

    add snpl image, use styled-components + css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-// src/App.js
 import React from "react";
 import BlogPost from "./BlogPost";
 
 const posts = [
   {
-    title: "My First Post",
-    content: "This is the content of the first post.",
-    imageUrl: "https://github.com/kyanrussell/kyanocitta/releases/download/0.1/DSC09961.JPG",
-  },
-  {
-    title: "Another Post",
-    content: "Here's some more content.",
-    imageUrl: "https://github.com/your-username/your-repo/releases/download/v1.0/image2.jpg",
+    title: "Snowy Plovers",
+    content: `
+    # How much markdown can I use?
+    Probably not much.
+    `,
+    imageUrl: "/kyanocitta/images/snpl.png",
   },
 ];
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>Welcome to My Blog</h1>
-      </header>
-      <main>
-        {posts.map((post, index) => (
-          <BlogPost key={index} {...post} />
-        ))}
-      </main>
-      <footer>
-        <p>&copy; 2025 My Blog</p>
-      </footer>
+    <div className="w-screen h-screen">
+      {posts.map((post, index) => (
+        <BlogPost key={index} {...post} />
+      ))}
     </div>
   );
 }
--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,15 +1,30 @@
-// src/BlogPost.js
 import React from "react";
+import './BlogPost.css';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-family: 'Arial', sans-serif;
+  color: green;
+`;
+
+const Paragraph = styled.p`
+  font-size: 1rem;
+  line-height: 1.6;
+  margin-bottom: 16px;
+`;
 
 const BlogPost = ({ title, content, imageUrl }) => {
   return (
-    <div className="post">
-      {imageUrl && <img src={imageUrl} alt={title} />}
-      <h2>{title}</h2>
-      <p>{content}</p>
+    <div className="blog-post-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={title} />
+      </div>
+      <div className="content">
+        <Heading>{title}</Heading>
+        <Paragraph>{content}</Paragraph>
+      </div>
     </div>
   );
 };
 
 export default BlogPost;
-
