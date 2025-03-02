4b9f2b7b3ee50f9c15db797d1f2c8dba73ae88d6
Kyan Russell <ksrussell@berkeley.edu>
2025-03-02 11:23:58 -0800

    remove swp files and update css

--- src/posts.js
+++ src/posts.js
@@ -22,6 +22,29 @@ const posts = [
     type: 'body',
     content: 'This bird has black, orange, pink, red, which can be read as ko:pr.'
   },
+],
+[
+  {
+    type: 'title',
+    content: 'Reading Snowy Plover Bands',
+  },
+  {
+    type: 'body',
+    content: 'Read the colors from left to right, top-down - always from the bird\'s perspective.'
+  },
+  {
+    type: 'heading',
+    content: 'Example',
+  },
+  {
+    type: 'image',
+    src: "/kyanocitta/images/snpl.png",
+    alt: 'Another image',
+  },
+  {
+    type: 'body',
+    content: 'This bird has black, orange, pink, red, which can be read as ko:pr.'
+  },
 ]
 ];
 
