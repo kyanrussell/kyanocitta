f93d853cd661633a32ce6f1ef339a5bfda8861ec
Kyan Russell <ksrussell@berkeley.edu>
2025-06-18 11:34:20 -0700

    update About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,23 @@
+import { BlogPostContainer, Title, Body} from 'components/Styles'
 
 function About() {
   return (
-    <div>
-    <a href="https://www.instagram.com/kyanocitta"> Instagram </a>
-    </div>
+    <BlogPostContainer>
+    <Title> Kyan Russell </Title>
+	    <Body>
+		    I'm a Monterey native passionate about birds, native plants, art, and photography. I work primarily in pencil and watercolors.
+		    I especially love to learn and share about range-restricted species and tricky identifications.
+		I share artworks on Instagram @
+		    <a href="https://www.instagram.com/kyanocitta">kyanocitta</a>.
+	    </Body>
+	    <Body>
+		    I serve on the board of Monterey Audubon Society, for which I also lead birding field trips.
+		</Body>
+	    <Body>
+	    	Questions or feedback can be directed to kyansrussell@gmail.com.
+	    </Body>
+    </BlogPostContainer>
   );
 }
 
 export default About;
-
