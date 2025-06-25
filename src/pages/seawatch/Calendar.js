8f6a9012e7eefc0060215d6ada4b480bce498f22
Kyan Russell <ksrussell@berkeley.edu>
2025-06-24 21:04:29 -0700

    sort calendar by current month

--- src/pages/seawatch/Calendar.js
+++ src/pages/seawatch/Calendar.js
@@ -1,58 +1,98 @@
 import { BlogPostContainer, Title, Heading, Body} from 'components/Styles'
 
+const calendar = [
+	{
+		month: <Heading>January</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+	{
+		month: <Heading>February</Heading>,
+		content: <Body>
+			Black-legged kittiwake incursion.
+		</Body>
+	},
+	{
+		month: <Heading>March</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+	{
+		month: <Heading>April</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+	{
+		month: <Heading>May</Heading>,
+		content: <Body>
+			Hawaiian petrels.
+		</Body>
+	},
+	{
+		month: <Heading>June</Heading>,
+		content: <Body>
+			Puffins, petrels, sooty shearwaters, subadult jaegers.			
+		</Body>
+	},
+	{
+		month: <Heading>July</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+	{
+		month: <Heading>August</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+	{
+		month: <Heading>September</Heading>,
+		content: <Body>
+		    Arctic terns. Buller's shearwater.
+		</Body>
+	},
+	{
+		month: <Heading>October</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+	{
+		month: <Heading>November</Heading>,
+		content: <Body>
+			Scoters, loons.
+		</Body>
+	},
+	{
+		month: <Heading>December</Heading>,
+		content: <Body>
+			-
+		</Body>
+	},
+]
+
+const d = new Date();
+let month = d.getMonth();
+const monthsAfterCurrent = calendar.slice(month);
+const monthsBeforeCurrent = calendar.slice(0, month);
 
 function Calendar() {
+
   return (
     <BlogPostContainer>
     <Title> Calendar </Title>
-	    <Heading> June </Heading>
-	    <Body>
-		    Puffins, petrels, sooty shearwaters, subadult jaegers.
-		</Body>
-	    <Heading> July </Heading>
-	    <Body>
-		    
-		</Body>
-	    <Heading> August </Heading>
-	    <Body>
-		    
-		</Body>
-	    <Heading> September </Heading>
-	    <Body>
-		    Arctic terns. Buller's shearwater.
-		</Body>
-	    <Heading> October </Heading>
-	    <Body>
-		    
-		</Body>
-	    <Heading> November </Heading>
-	    <Body>
-		    Scoters, loons.
-		</Body>
-	    <Heading> December </Heading>
-	    <Body>
-		    
-		</Body>
-	    <Heading> January </Heading>
-	    <Body>
-		    
-		</Body>
-	    <Heading> February </Heading>
-	    <Body>
-		    Black-legged kittiwake incursion.
-		</Body>
-	    <Heading> March </Heading>
-	    <Body>
-		    
-		</Body>
-	    <Heading> April </Heading>
-	    <Body>
-		     
-		</Body>
-	    <Heading> May </Heading>
-	    <Body>
-		    Hawaiian petrels.
-		</Body>
+	    <div>
+	      {[...monthsAfterCurrent, ...monthsBeforeCurrent].map((item, index) => (
+	        <div key={index}>
+	          {item.month}
+	          {item.content}
+	        </div>
+	      ))}
+	    </div>
     </BlogPostContainer>
   );
 }
