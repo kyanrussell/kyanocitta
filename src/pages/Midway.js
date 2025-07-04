a2bed31122f93cc8b3a8315c4113224cb3be6bdc
Kyan Russell <ksrussell@berkeley.edu>
2025-07-04 11:55:40 -0700

    formatting

--- src/pages/Midway.js
+++ src/pages/Midway.js
@@ -1,27 +1,44 @@
-import {Heading, Body, BlogPostContainer, Image, H3 } from 'components/Styles';
+import {Heading, Body, BlogPostContainer, Image, ImageContainer, H3 } from 'components/Styles';
 
 function Midway() {
   return (
     <BlogPostContainer>
 
+	<Heading> Introduction </Heading>
+	<Body>
+	In the winter of 2024 I was fortunate enough to participate as a volunteer counter for the annual albatross census on Midway Atoll. The following is an account of my 6-week experience on the largest albatross colony on Earth.
+	</Body>
+
 
 	<Heading> 2024-12-17 </Heading>
-	<Body>The crew didn't fill the jet with enough fuel, so we're waiting on that... Only a few minutes behind, which by Midway (or remote island) standards is apparently pretty good. Beth Flint picked us up in her car and drove like a demon to the FWS bunkhouse. We picked up a van and two biologists who are conducting a drone survey. Beth was very nice and bought us all dinner and told some funny stories (like how the Idaho FWS staff sent a bunch of office furniture + TVs to Honolulu but forgot to the lash them down for the container ship). 10 on board: pilot, co-pilot, Zhao (sp.?)  - Midway staff, Anna + Dan - biologists, and volunteers: me, Ken, Simone, Lauren, Ashley. The jet we're on is fancy but small - like that of a low-tier CEO. Buttons don't work. As we were waiting for our luggage to be packed, a PAGP flew in over our heads giving a piercing call and landed in the parking lot. </Body>
+	<Body>
+	The crew didn't fill the jet with enough fuel, so we're waiting on that... Only a few minutes behind, which by Midway (or remote island) standards is apparently pretty good. Beth Flint picked us up in her car and drove like a demon to the FWS bunkhouse. We picked up a van and two biologists who are conducting a drone survey. Beth was very nice and bought us all dinner and told some funny stories (like how the Idaho FWS staff sent a bunch of office furniture + TVs to Honolulu but forgot to the lash them down for the container ship). 10 on board: pilot, co-pilot, Zhao (sp.?)  - Midway staff, Anna + Dan - biologists, and volunteers: me, Ken, Simone, Lauren, Ashley. The jet we're on is fancy but small - like that of a low-tier CEO. Buttons don't work. As we were waiting for our luggage to be packed, a PAGP flew in over our heads giving a piercing call and landed in the parking lot.
+	</Body>
 
-	<Image src="/kyanocitta/images/page1_pagp.png"></Image>
+	<ImageContainer>
+		<Image src="/kyanocitta/images/page1_pagp.png"></Image>
+	</ImageContainer>
 
 	<Body>Moonrise behind us</Body>
 		<Body>waning gibbous</Body>
 			<Body>burnt orange under clouds</Body>
 
-	<Image src="/kyanocitta/images/page1_window_view.png"></Image>
+	<ImageContainer>
+		<Image src="/kyanocitta/images/page1_window_view.png"></Image>
+	</ImageContainer>
 
-	<Body>1 hour to go... Moon is higher. Shining white, but reflecting orange off the ocean. Water looks very calm.. infinite expanse of silver sheet. haze at the horizon - a few bright stars.</Body>
+	<Body>
+	1 hour to go... Moon is higher. Shining white, but reflecting orange off the ocean. Water looks very calm.. infinite expanse of silver sheet. haze at the horizon - a few bright stars.
+	</Body>
 
-	<Body>After landing:</Body>
-	<Body>Feels like a fever dream. First the descent - plane pointed down after sharp bank. Runway lights out of nowhere. albatross like rows of tombstones off runway. Got off the plane - BOPE overhead like bats glowing in moonlight. Whinnying of LAAL. Bill clacks. Golf cart through the colony to Charlie barracks. Welcome from staff and counters.</Body>
-
-	<Body>Meet at 6:20 in lobby with headlamps.</Body>
+	<Body>
+	After landing:
+	</Body>
+	<Body>
+	Feels like a fever dream. First the descent - plane pointed down after sharp bank. Runway lights out of nowhere. albatross like rows of tombstones off runway. Got off the plane - BOPE overhead like bats glowing in moonlight. Whinnying of LAAL. Bill clacks. Golf cart through the colony to Charlie barracks. Welcome from staff and counters.</Body>
+	<Body>
+	Meet at 6:20 in lobby with headlamps.
+	</Body>
 
 
     <Heading> 2024-12-18 </Heading>
